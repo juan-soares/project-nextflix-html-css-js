2ec8a2a60f2753c674548c00db619595ded4143e
juan-soares <juan.css013@gmail.com>
2024-07-15 08:59:49 -0300

    refactor: using functions.

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,16 +1,22 @@
 const botoes = document.querySelectorAll(".botao");
 const personagens = document.querySelectorAll(".personagem");
 
+function desselecionarBotao() {
+  const botaoSelecionado = document.querySelector(".botao.selecionado");
+  botaoSelecionado?.classList.remove("selecionado");
+}
+
+function desselecionarPersonagem() {
+  const personagemSelecionado = document.querySelector(
+    ".personagem.selecionado"
+  );
+  personagemSelecionado?.classList.remove("selecionado");
+}
+
 botoes.forEach((botao, index) => {
   botao.addEventListener("click", () => {
-    const botaoSelecionado = document.querySelector(".botao.selecionado");
-    const personagemSelecionado = document.querySelector(
-      ".personagem.selecionado"
-    );
-
-    botaoSelecionado?.classList.remove("selecionado");
-    personagemSelecionado?.classList.remove("selecionado");
-
+    desselecionarBotao();
+    desselecionarPersonagem();
     botao.classList.add("selecionado");
     personagens[index].classList.add("selecionado");
   });
