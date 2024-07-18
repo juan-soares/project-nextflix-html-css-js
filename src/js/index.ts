5dcec25323086f69226063ec6189790dc25f358c
juan-soares <juan.css013@gmail.com>
2024-07-17 23:31:32 -0300

    refactor: updated functions name to English.

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,20 +1,20 @@
 const buttons = document.querySelectorAll(".button");
 const cards = document.querySelectorAll(".technology__card");
 
-function desselecionarbutton() {
+function resetButtonSelection() {
   const selectedButton = document.querySelector(".button--selected");
   selectedButton?.classList.remove("button--selected");
 }
 
-function desselecionartechnology() {
+function resetCardSelection() {
   const selectedCard = document.querySelector(".technology__card--selected");
   selectedCard?.classList.remove("technology__card--selected");
 }
 
 buttons.forEach((button, index) => {
   button.addEventListener("click", () => {
-    desselecionarbutton();
-    desselecionartechnology();
+    resetButtonSelection();
+    resetCardSelection();
     button.classList.add("button--selected");
     cards[index].classList.add("technology__card--selected");
   });
